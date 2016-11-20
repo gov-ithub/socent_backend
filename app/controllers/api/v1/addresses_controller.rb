f1a1a667a4b3566f248abbc3537be3f87e656428
Silviu Straliciuc <silviu.straliciuc@gmail.com>
2016-11-20 16:13:29 +0200

    Addresses Controller create

--- app/controllers/api/v1/addresses_controller.rb
+++ app/controllers/api/v1/addresses_controller.rb
@@ -3,4 +3,28 @@ class Api::V1::AddressesController < ApplicationController
   def show
     respond_with Address.find params[:id]
   end
+
+  def create
+    @address = Address.create!(create_params)
+    respond_with @address, status: :created
+  end
+
+  # location response wants 'address' path helpers
+  def address_url(address)
+    api_v1_address_url(address)
+  end
+
+  private
+
+  def create_params
+    params.permit(
+      :city,
+      :street,
+      :street_number,
+      :phone,
+      :fax,
+      :email,
+      :district_id)
+  end
+
 end
