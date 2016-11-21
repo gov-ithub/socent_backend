04d1d355440b1f3594bd514d5d921d5f707e8651
Silviu Straliciuc <silviu.straliciuc@gmail.com>
2016-11-21 12:12:02 +0200

    Create upload model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161120040407) do
+ActiveRecord::Schema.define(version: 20161121100917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,13 @@ ActiveRecord::Schema.define(version: 20161120040407) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.index ["social_intervention_domain_category_id"], name: "social_intervention_domains_category_id", using: :btree
+  end
+
+  create_table "uploads", force: :cascade do |t|
+    t.text     "path"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "vulnerable_groups", force: :cascade do |t|
